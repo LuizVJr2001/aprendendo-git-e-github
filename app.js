34444523391432a95704636007059973c15cda6f
LuizVJr2001 <you@example.com>
2023-04-03 16:29:11 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Este é uma alteração criada por um colaborador do repositório");
 
 console.log("Essa é uma linha de código escrita por um colaborador");
+
+console.log("Enviando para o Github");
