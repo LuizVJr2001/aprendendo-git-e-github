e432bb75bd0afb2c09d6a24d3ee717db85de054c
LuizVJr2001 <you@example.com>
2023-04-06 18:21:08 -0300

    Atualizando app.js

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ console.log("Este é uma alteração criada por um colaborador do repositório")
 console.log("Essa é uma linha de código escrita por um colaborador");
 
 console.log("Enviando para o Github");
+
+console.log("teste aula 4");
